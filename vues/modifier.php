5aa0fa968fe48551a19cb001a9133ce63b22e611
Carolanne de Chantal <e2195459@cmaisonneuve.qc.ca>
2023-01-11 17:20:30 -0500

    ajout vue de modification

---
+++ vues/modifier.php
@@ -0,0 +1,34 @@
+<h1>Formulaire modification du cellier</h1>
+<form action="/ajouterBouteilleCellier" method="POST">
+    <div>   
+        <label for="">date d'achat</label>
+    <input type="text" name="date_achat" id="">
+    </div>
+    <div>   
+        <label for="">garde jusqua</label>
+    <input type="text" name="garde_jusqua" id="">
+    </div>
+    <div>  
+         <label for="">notes</label>
+    <input type="text" name="notes" id="">
+    </div>
+    <div>   
+        <label for="">prix</label>
+    <input type="text" name="prix" id="">
+    </div>
+    <div>  
+         <label for="">quantite</label>
+    <input type="text" name="quantite" id="">
+    </div>
+    <div>  
+         <label for="">millesime</label>
+    <input type="text" name="millesime" id="">
+    </div>
+    <div>  
+         <label for="">choisir bouteille</label>
+    <input type="text" name="id_bouteille" id="">
+    </div>
+    <input type="submit">
+    <!-- rendre le choix dynamic avec un select -->
+
+</form>
