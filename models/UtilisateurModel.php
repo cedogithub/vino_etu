3b65dd3db3b3e6d07f9f24817abc42802844fdfe
cedogithub <39746523+cedogithub@users.noreply.github.com>
2023-01-16 15:23:21 -0500

    creation du model utilisateur

---
+++ models/UtilisateurModel.php
@@ -0,0 +1,37 @@
+<?php
+/* */
+
+class UtilisateurModel extends Modele {
+
+    public function getUsager($courriel){
+        $result = $this->database->fetch("SELECT * FROM utilisateur WHERE uti_courriel = '$courriel'");
+        return $result;
+    }
+    // public function getUsager($courriel){
+
+    // }
+
+    public function creerUsager($data){
+            
+        $this->database->query('INSERT INTO utilisateur ?', [ 
+            'uti_id' => $data['uti_id'],
+			'uti_courriel' => $data['uti_courriel'],
+			'uti_mdp' => $data['uti_mdp'],
+			'uti_nom' => $data['uti_nom'],
+			'uti_prenom' => $data['uti_prenom'],
+			'uti_dcc' => $data['uti_dcc'],
+			'uti_rol_id	' => $data['uti_rol_id	']// here can be omitted question mark
+            
+        ]);
+        // INSERT INTO users (`name`, `year`) VALUES ('Jim', 1978)
+        
+      
+    }
+    // INSERT INTO users (`name`, `year`) VALUES ('Jim', 1978)
+    
+ 
+
+}
+
+
+?>
