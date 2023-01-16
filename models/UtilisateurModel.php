ceef738781161877d218e4d7cd6cb0d6d71ae628
cedogithub <39746523+cedogithub@users.noreply.github.com>
2023-01-16 15:28:29 -0500

    creation de la fonction getUsager dans utilisateurModele

--- models/UtilisateurModel.php
+++ models/UtilisateurModel.php
@@ -10,25 +10,6 @@ class UtilisateurModel extends Modele {
     // public function getUsager($courriel){
 
     // }
-
-    public function creerUsager($data){
-            
-        $this->database->query('INSERT INTO utilisateur ?', [ 
-            'uti_id' => $data['uti_id'],
-			'uti_courriel' => $data['uti_courriel'],
-			'uti_mdp' => $data['uti_mdp'],
-			'uti_nom' => $data['uti_nom'],
-			'uti_prenom' => $data['uti_prenom'],
-			'uti_dcc' => $data['uti_dcc'],
-			'uti_rol_id	' => $data['uti_rol_id	']// here can be omitted question mark
-            
-        ]);
-        // INSERT INTO users (`name`, `year`) VALUES ('Jim', 1978)
-        
-      
-    }
-    // INSERT INTO users (`name`, `year`) VALUES ('Jim', 1978)
-    
  
 
 }
