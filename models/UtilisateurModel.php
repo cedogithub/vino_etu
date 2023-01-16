c93d5d557908c325469aa62a660ac16a894be413
cedogithub <39746523+cedogithub@users.noreply.github.com>
2023-01-16 15:52:04 -0500

    creation de la fonction creerUsager dans utilisateurModele

--- models/UtilisateurModel.php
+++ models/UtilisateurModel.php
@@ -10,6 +10,22 @@ class UtilisateurModel extends Modele {
     // public function getUsager($courriel){
 
     // }
+
+    public function creerUsager($data){
+            
+        $this->database->query('INSERT INTO utilisateur ?', [ 
+            'uti_id' => $data['uti_id'],
+			'uti_courriel' => $data['uti_courriel'],
+			'uti_mdp' => $data['uti_mdp'],
+			'uti_nom' => $data['uti_nom'],
+			'uti_prenom' => $data['uti_prenom'],
+			'uti_dcc' => $data['uti_dcc'],
+			'uti_rol_id	' => $data['uti_rol_id	']// here can be omitted question mark
+            
+        ]);
+        
+      
+    }    
  
 
 }
