928ec06ea7192b6c89c44e3e63690d3ef8b99fe4
kennethEdwin147 <edwingbeti@gmail.com>
2023-01-19 22:19:03 -0500

    implimentation des routes du controlleur utilisateur

--- Controllers/Utilisateur.php
+++ Controllers/Utilisateur.php
@@ -1,7 +1,17 @@
 <?php
 class Utilisateur
 {
+    public function accueil()
+    {
+        $this->render('utilisateur/connexion');  
+    }
 
+    public function inscription()
+    {   
+        $this->render('utilisateur/inscription');  
+    }
+
+  
     public function deconnexion()
     {
         unset($_SESSION['utilisateur']);
@@ -9,6 +19,28 @@ class Utilisateur
     }
 
 
+
+
+
+
+
+          
+    /**
+     * Affiche la page demandée 
+     *
+     * @param  string $file_name
+     * @param  object|object[]
+     * @return void
+     */
+    public function render($file_name, $data = [])
+    {
+        $loader = new \Twig\Loader\FilesystemLoader(dirname(__DIR__) . '/template');
+        $twig = new \Twig\Environment($loader, [
+            'cache' => false
+        ]);
+        $twig->addGlobal('session', $_SESSION);
+        echo $twig->render($file_name.'.html', $data);
+    }
 }
 
 
--- index.php
+++ index.php
@@ -2,33 +2,35 @@
 session_start();
 require_once __DIR__. '/dataconf.model.php';
 require __DIR__ . '/vendor/autoload.php';
-require __DIR__ . '/models/UtilisateurModel.php';
 // Create Router instance
 $router = new \Bramus\Router\Router();
 
 
-$router->get('/', 'Bouteille@accueil');
-$router->get('/accueil', 'Bouteille@accueil');
-$router->get('/nouvelleBouteilleCellier', 'Bouteille@nouvelleBouteilleCellier');
 
-$router->post('/ajouterBouteilleCellier', 'Bouteille@ajouterBouteilleCellier');
-$router->get('/modificationBouteille/{id}', 'Bouteille@modificationBouteille');
+/* route de sécurité: before*/
 
-
-$router->post('/boireQuantiteBouteille', 'Bouteille@boireQuantiteBouteille');
-$router->post('/ajouterQuantiteBouteille', 'Bouteille@ajouterQuantiteBouteille');
-$router->post('/ajouterQuantiteBouteille', 'Bouteille@ajouterQuantiteBouteille');
-
-$utilisateur = new UtilisateurModel();
-print_r($utilisateur->getUsager());
+$router->get('/', 'Utilisateur@accueil');
+$router->get('/utilisateur/accueil', 'Utilisateur@accueil');
+$router->get('/utilisateur/inscription', 'Utilisateur@inscription');
 
 
+$router->post('/utilisateur/connection', 'Utilisateur@connection');
+$router->get('/utilisateur/deconnexion', 'Utilisateur@deconnexion');
+$router->post('/utilisateur/creation', 'Utilisateur@connection');
 
-$router->post('/connection', 'Utilisateur@connection');
-$router->get('/deconnexion', 'Utilisateur@deconnexion');
+
+/* route de sécurité: before */
+
+$router->get('/bouteille/accueil', 'Bouteille@accueil');
+$router->get('/bouteille/nouveau', 'Bouteille@nouvelleBouteilleCellier');
+$router->post('/bouteille/ajouter', 'Bouteille@ajouterBouteilleCellier');
+$router->get('/bouteille/modifier/{id}', 'Bouteille@modificationBouteille');
+$router->post('/bouteille/quantite/boire/', 'Bouteille@boireQuantiteBouteille');
+$router->post('/bouteille/quantite/ajouter/', 'Bouteille@ajouterQuantiteBouteille');
 
 $router->run();
-=======
+
+
 // SAQ
 /* for ($i=1; $i < 10 ; $i++) { 
 }
