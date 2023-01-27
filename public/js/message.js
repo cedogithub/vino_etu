d6cbeb9959f11d18e2b0031a2e514b766908d1a3
kennethEdwin147 <edwingbeti@gmail.com>
2023-01-26 22:11:38 -0500

    amélioration du message de confirmation

--- public/js/message.js
+++ public/js/message.js
@@ -1,9 +1,18 @@
 let messageBox = document.querySelector('#alert-5');
-messageBox.addEventListener('onBlur', ()=>{
-    messageBox.remove()
-})
-let xButton = document.querySelector('[data-dismiss-target]')
-xButton.addEventListener('click', ()=>{
-    console.log('x')
-    messageBox.remove();
-})
+
+/* parfois message box est pas la  !
+ex: quand il est sur son cellier sans avoir rien modifier ou ajouter
+il y aura une erreur messagebox not defined */
+if (messageBox) {
+    messageBox.addEventListener('onBlur', ()=>{
+        messageBox.remove()
+        window.location.href = '/bouteille/cellier';
+    })
+    let xButton = document.querySelector('[data-dismiss-target]')
+    xButton.addEventListener('click', ()=>{
+        console.log('x')
+        messageBox.remove();
+        window.location.href = '/bouteille/cellier';
+    })
+    
+}
