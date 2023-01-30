af19f2f231030f2c5a671a957bbba13b20a5ba99
cedogithub <39746523+cedogithub@users.noreply.github.com>
2023-01-29 19:07:28 -0500

    creation de la requete qui pour compter le nombre dusager

--- models/AdminModele.php
+++ models/AdminModele.php
@@ -1,8 +1,26 @@
 <?php
 
-class UtilisateurModel extends Modele {
+class AdminModele extends Modele {
+
+ /**
+     * Cette methode sert a selectionner un utilisateur de la BD
+     */
+    public function getAdmin(){
+        $result = $this->database->fetch("SELECT * FROM utilisateur WHERE uti_rol_id = '2'");
+        return $result;
+    }
+
+    
+    public function getNombreUsagers(){
+        $result = $this->database->fetch("SELECT COUNT(*)
+        FROM utilisateur;");
+        return $result;
+    }
+
 
 
 
 
 }
+
+?>
