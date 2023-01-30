55d6247754a19d6f984e760d69e615b57377a99f
cedogithub <39746523+cedogithub@users.noreply.github.com>
2023-01-29 19:08:57 -0500

    changemant de nom, methode dashboard nouvelle variable nombreUti

--- controllers/Admin.php
+++ controllers/Admin.php
@@ -1,39 +1,18 @@
 <?php
 
-class admin
+
+class Admin
 {
-
-public function admin()
-    {  
-        $this->render('admin/dashboard.html');
-    }
     
-    /**
-     * Tente l'ouverture d'une connexion : si réussi, redirige vers  
-     * et sinon, réaffiche le formulaire de connexion avec un message d'erreur.
-     */
-    public function connexion()
-    {
-        $erreur = false;
-        $courriel = $_POST['uti_courriel'];
-        $mdp = $_POST['uti_mdp'];
-        $model = new UtilisateurModel();
 
-        $utilisateur = $model->getUsager($courriel); 
-    
-        if(!$erreur) {
-            if($utilisateur['uti_rol_id'] == '2'){
-            header("Location: /admin/dashboard"); 
-            exit();
-            }
-        }
-        else {
-            $this->render('utilisateur/connexion.html', [
-                "erreur" => $erreur
-            ]);  
-        }
+     public function dashboard(){
+        $model = new AdminModele();
+        $nombreUti = $model-> getNombreUsagers();
+
+        $this->render('admin/dashboard.html', [
+            'nombreUti' => $nombreUti
+        ]);
      }
-
 
     /* Affiche la page demandée 
     *
