4a4ecd2a9e9635e09db595236c7a1ac90d1a0a82
cedogithub <39746523+cedogithub@users.noreply.github.com>
2023-01-31 15:55:18 -0500

    Ajout de commentaire dans quelques fonctions

--- models/AdminModele.php
+++ models/AdminModele.php
@@ -10,13 +10,17 @@ class AdminModele extends Modele {
         return $result;
     }
 
-    
+    /**
+     * Affiche le nombre de usagers total dans la BD
+     */
     public function getNombreUsagers(){
         $result = $this->database->fetch("SELECT COUNT(*) as nbUsager
         FROM utilisateur;");
         return $result['nbUsager'];
     }
-    
+    /**
+     * Affiche le nombre de celliers total dans la BD
+     */
     public function getNombreCelliers(){
         $result = $this->database->fetch("SELECT COUNT(*) as nbCelliers
         FROM cellier;");
@@ -30,11 +34,7 @@ class AdminModele extends Modele {
         return $result;
     }
 
-    public function getNbBouteille24h(){
-        $result = $this->database->fetch("SELECT COUNT(*) FROM bouteille_du_cellier WHERE bdc_date_achat > DATE_SUB(NOW(), INTERVAL 24 HOUR)");
-        return $result;
-    }
-
+    
     public function getNbParCelliers(){
         $cellier = $this->database->fetchAll("SELECT *
         FROM cellier");
@@ -46,10 +46,17 @@ class AdminModele extends Modele {
     }
 
     public function findBouteilleParUsers(){
-            return $this->database->fetchAll("SELECT COUNT(bouteille_du_cellier.bdc_bout_id) as nbBouteille, cellier.cel_id FROM `cellier` JOIN bouteille_du_cellier on cellier.cel_id = bouteille_du_cellier.bdc_cel_id group by cellier.cel_id;");
+        return $this->database->fetchAll("SELECT COUNT(bouteille_du_cellier.bdc_bout_id) as nbBouteille, cellier.cel_id FROM `cellier` JOIN bouteille_du_cellier on cellier.cel_id = bouteille_du_cellier.bdc_cel_id group by cellier.cel_id;");
         
     }
 
+    /**
+     * Affiche le nombre de bouteilles ajouter il ya 24h
+     */
+    public function getNbBouteille24h(){
+        $result = $this->database->fetch("SELECT COUNT(*) FROM bouteille_du_cellier WHERE bdc_date_achat > DATE_SUB(NOW(), INTERVAL 24 HOUR)");
+        return $result;
+    }
 }
 
 ?>
